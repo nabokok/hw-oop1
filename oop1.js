351df7c17892874ec6ae99d71f229bc48cf171e8
Alyona <alyonana42@gmail.com>
2022-12-15 19:51:53 +0200

    add properties

--- oop1.js
+++ oop1.js
@@ -16,10 +16,11 @@ class Developer extends TeamMember {
 
 class FrontEndDev extends Developer {
     #level
-    constructor(name, lang, level, code, salary) {
+    constructor(name, lang, framework, level, code, salary) {
         super(code, salary)
         this.name = name
         this.lang = lang
+        this.framework = framework
         this.#level = level
         this.occupation = 'Front-end developer'
     }
@@ -30,12 +31,14 @@ class FrontEndDev extends Developer {
 
 class BackEndDev extends Developer{
     #level
-    constructor(name, lang, level, code, salary) {
+    constructor(name, lang, os, level, code, salary) {
         super(code, salary)
         this.name = name
         this.lang = lang
+        this.os = os
         this.#level = level
         this.occupation = 'Backend developer'
+        this.os = os
     }
     intro = () => console.log(`Hello! My name is ${this.name}. I'm ${this.occupation}.`);
     sayLevel = () => console.log(this.level);
@@ -43,11 +46,12 @@ class BackEndDev extends Developer{
 
 class QA extends TeamMember {
     #level
-    constructor(name, level, salary) {
+    constructor(name, level, isAutomation, salary) {
         super(salary)
         this.name = name
         this.#level = level
         this.occupation = 'QA Engineer'
+        this.isAutomation = isAutomation;
     }
     testing = () => console.log(`A new bug has been opened!`);
     intro = () => console.log(`Hello! My name is ${this.name}. I'm ${this.occupation}.`);
@@ -55,23 +59,24 @@ class QA extends TeamMember {
 
 class PM extends TeamMember {
     #level
-    constructor(name, level, salary) {
+    constructor(name, level, isTechBackground, salary) {
         super(salary)
         this.name = name
         this.#level = level
         this.occupation = 'Project manager'
+        this.isTechBackground = isTechBackground
     }
     asking = () => console.log(`What are your statuses?`);
     intro = () => console.log(`Hello! My name is ${this.name}. I'm ${this.occupation}.`);
     sayLevel = () => console.log(this.level);
 }
 
-const feDev1 = new FrontEndDev('Mykola', 'Java Script', 'Middle', `<div class="greeting">Hello, world!</div>`, `4000$`);
-const beDev1 = new BackEndDev('Petro', 'Ruby', 'Middle', `<<-SQL
+const feDev1 = new FrontEndDev('Mykola', 'Java Script', 'React', 'Middle', `<div class="greeting">Hello, world!</div>`, `4000$`);
+const beDev1 = new BackEndDev('Petro', 'Ruby', 'Linux', 'Middle', `<<-SQL
 INNER JOIN (#{transfer_receivers_uniq_query}) AS "trs_uniq" ON "trs_uniq"."utps_id" = "utps"."id"
 SQL`, `4000$`);
-const qa1 = new QA('Stepan', 'Junior', `2000$`)
-const pm1 = new PM('Vasyl', 'Senior', `8000$`)
+const qa1 = new QA('Stepan', 'Junior', false, `2000$`)
+const pm1 = new PM('Vasyl', 'Senior', true, `8000$`)
 
 feDev1.coding();
 beDev1.coding();
